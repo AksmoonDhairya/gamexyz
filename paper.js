ee9ded4d0e5457e8b162856534a9ce15943068c5
AksmoonDhairya <80187969+AksmoonDhairya@users.noreply.github.com>
2021-04-10 15:16:57 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,38 @@
+class Paper{
+	constructor(x,y,r)
+	{
+		this.image=loadImage("sprites/paper.png")
+   
+		var options={
+		
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
+
+			
+			}
+		
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;		
+
+			push()
+			fill("blue")
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			
+			ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+
+}
